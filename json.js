8bb982beb85b001c9eeaf3bd7d1daac008375061
rakibul972500 <rakibul972500@gmail.com>
2024-09-30 23:20:00 +0600

    first commit

---
+++ json.js
@@ -0,0 +1,5 @@
+// js object Notation (JSON)
+ const user={id:23, name:'Kodom Ali', age:53, address:'Gulistan'}
+ const strigified=JSON.stringify(user);
+ console.log(user)
+ console.log(strigified);
